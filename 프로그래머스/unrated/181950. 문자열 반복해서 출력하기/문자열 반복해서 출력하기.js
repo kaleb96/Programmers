3d90729ef7cb7ef1e82a1d282f3b02c9d689534a
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-11-16 14:20:47 +0900

    [level unrated] Title: 문자열 반복해서 출력하기, Time: 46.59 ms, Memory: 32.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.js
+++ 프로그래머스/unrated/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.js
@@ -11,12 +11,8 @@ rl.on('line', function (line) {
 }).on('close', function () {
     str = input[0];
     n = Number(input[1]);
-    let result= '';
-    if((str.length >= 1 && str.length <= 10) && n >= 1 && n <= 5) {
-        for(let i = 0; i < n; i++) {
-            result = result.concat(str);
-        }
-            console.log(result);
+    if((str.length >= 1 && str.length <= 10) && (n >= 1 && n <= 5)) {
+        console.log(str.repeat(n));
     }
     
 });
