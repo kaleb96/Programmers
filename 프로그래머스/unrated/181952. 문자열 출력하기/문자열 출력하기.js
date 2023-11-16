4362c45ee8dbdcc302bd804f22b90a7d7089cc9f
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-11-16 10:20:21 +0900

    [level unrated] Title: 문자열 출력하기, Time: 43.94 ms, Memory: 32.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181952. 문자열 출력하기/문자열 출력하기.js
@@ -0,0 +1,16 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let input = [];
+
+rl.on('line', function (line) {
+    input = [line];
+}).on('close',function(){
+    str = input[0];
+    if(str.length >= 1 && str.length <= 1000000) {
+        console.log(str);
+    }
+});
