d23193c29497b83b8a7cd646878f398858f921ed
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-11-17 10:24:38 +0900

    [level unrated] Title: 특수문자 출력하기, Time: 48.81 ms, Memory: 32.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181948. 특수문자 출력하기/특수문자 출력하기.js
@@ -0,0 +1,9 @@
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.on('close', function () {
+  console.log('!@#$%^&*\(\\\'\"<>?:\;');  
+});
