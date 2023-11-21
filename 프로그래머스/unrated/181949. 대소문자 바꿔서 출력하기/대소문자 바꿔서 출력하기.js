b41c0541c175ace0ed11952fea78fd474bf7f1e3
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-11-21 18:25:22 +0900

    Update 대소문자 바꿔서 출력하기.js

--- 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
+++ 프로그래머스/unrated/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.js
@@ -5,18 +5,26 @@ const rl = readline.createInterface({
 });
 
 let input = [];
-let tmp = "";
+let tmp = '';
 rl.on('line', function (line) {
     input = [line];
 }).on('close',function(){
-    str = input[0].split('');
-
-    str.forEach((value, index) => {
-        if( value === value.toUpperCase()) {
-            str[index] = value.toLowerCase();
+    // str = input[0].split('');
+    // str.forEach((value, index) => {
+    //     if( value === value.toUpperCase()) {
+    //         str[index] = value.toLowerCase();
+    //     } else {
+    //         str[index] = value.toUpperCase();
+    //     }
+    // })
+    // console.log(str.join(''));
+    str = input[0];
+    for(let i =0; i< str.length; i++) {
+        if(str[i] === str[i].toUpperCase()) {
+            tmp += str[i].toLowerCase();
         } else {
-            str[index] = value.toUpperCase();
+            tmp += str[i].toUpperCase();  
         }
-    })
-    console.log(str.join(''));
+    }
+    console.log(tmp);
 });
