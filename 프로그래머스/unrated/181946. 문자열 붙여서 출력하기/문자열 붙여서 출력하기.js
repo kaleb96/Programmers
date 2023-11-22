a820862001ca6472c80cac31e1bd3d98c9cdca5f
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-11-22 16:11:51 +0900

    Update 문자열 붙여서 출력하기.js

--- 프로그래머스/unrated/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.js
+++ 프로그래머스/unrated/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.js
@@ -2,19 +2,20 @@ const readline = require('readline');
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-});
-
-let input = [];
-
-rl.on('line', function (line) {
-    input = line.split(' ');
-}).on('close', function () {
-    str1 = input[0];
-    str2 = input[1];
-    strx = str1+str2;
-    if(2<= strx.length && strx.length <= 20) {
-            console.log(strx);
-    }
+}).on('line', function(line) {
+    const strArr = line.split(' ')
+    console.log(strArr.join(''))
+})
     
-    
-});
+// 나의 풀이
+// let input = [];
+
+// rl.on('line', function (line) {
+//     input = line.split(' ');
+// }).on('close', function () {
+//     str1 = input[0];
+//     str2 = input[1];
+//     strx = str1+str2;
+//     if(2<= strx.length && strx.length <= 20) {
+//             console.log(strx);
+//     }
