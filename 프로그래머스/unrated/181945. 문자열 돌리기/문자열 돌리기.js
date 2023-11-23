7ad59f49dcad1926f77d744b21758f0946540f93
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-11-23 11:21:09 +0900

    [level unrated] Title: 문자열 돌리기, Time: 45.72 ms, Memory: 32.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.js
+++ 프로그래머스/unrated/181945. 문자열 돌리기/문자열 돌리기.js
@@ -9,11 +9,22 @@ let input = [];
 rl.on('line', function (line) {
     input = [line];
 }).on('close',function(){
-    str = input[0].split('');
-    if(1<= str.length && str.length <=10) {
-        str.forEach((value, idx) => {
-        console.log(value);
-    })
+    // forEach 문
+    // str = input[0].split('');
+    // if(1<= str.length && str.length <=10) {
+    //     str.forEach((value, idx) => {
+    //     console.log(value);
+    //     })
+    // }
+   
+    // ES6 spread 연산자
+    // str = input[0];
+    // [...str].forEach(c => console.log(c));
+    
+    // for...of 문
+    str = input[0]
+    for(let i of str) {
+        console.log(i);
     }
-
+    
 });
