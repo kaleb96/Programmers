84a9e5a8565596c9aa08ba45d5fd93b0a9b3c2b6
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-11-23 18:09:55 +0900

    [level unrated] Title: 더 크게 합치기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181939. 더 크게 합치기/더 크게 합치기.js
@@ -0,0 +1,10 @@
+function solution(a, b) {
+    var answer = 0;
+    let tmp1 = Number(`${a}${b}`);
+    let tmp2 = Number(`${b}${a}`);
+    
+    if(tmp1 >= tmp2) {
+        return tmp1;
+    } else 
+        return tmp2;
+}
