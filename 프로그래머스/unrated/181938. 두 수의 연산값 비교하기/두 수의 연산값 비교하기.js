5d2854e8a6a828790b8e98ce3c3cb85c42f216e2
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-11-24 10:12:14 +0900

    Update 두 수의 연산값 비교하기.js

--- 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
+++ 프로그래머스/unrated/181938. 두 수의 연산값 비교하기/두 수의 연산값 비교하기.js
@@ -1,8 +1,11 @@
 function solution(a, b) {
     
     let answer = '';
-    let check1 = Number(`${a}${b}`);
-    let check2 = 2 * Number(a * b);
-    answer = Math.max(check1, check2);
+   
+    if((a >= 1 && a <= 10000) && (b >= 1 && b <= 10000)) {
+        let check1 = Number(`${a}${b}`);
+        let check2 = 2 * Number(a * b);
+        answer = Math.max(check1, check2);
+    }
     return answer;
 }
