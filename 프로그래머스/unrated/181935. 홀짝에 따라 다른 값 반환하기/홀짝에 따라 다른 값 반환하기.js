0a26393879ec906d96da8c943156e106c1263558
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-11-29 11:16:43 +0900

    Update 홀짝에 따라 다른 값 반환하기.js

--- 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
+++ 프로그래머스/unrated/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -3,7 +3,7 @@ function solution(n) {
     // if(n>= 1 && n <= 100) { //제한사항
     //     if(n%2 === 1) { // 홀수의 경우
     //         for(let i = 1; i<=n; i +=2) {
-    //             answer += (i);
+    //             answer += i;
     //         }
     //     } else { // 짝수의 경우
     //         for(let i = 2; i<=n; i +=2) {
@@ -13,9 +13,10 @@ function solution(n) {
     //     return answer;  
     // }
     for(let i = 1; i <= n; i += 2) {
+        
         if(n % 2 === 1) { // 홀수의 경우
-            answer += i;
-        } else {
+            answer += i;  
+        } else { // 짝수의 경우
             answer += (i + 1) * (i + 1);
         }
     }
