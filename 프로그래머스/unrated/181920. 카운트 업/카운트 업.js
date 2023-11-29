3a823f787d1f168369441d9732aed8a31a62150c
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-11-29 13:20:25 +0900

    [level unrated] Title: 카운트 업, Time: 0.08 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181920. 카운트 업/카운트 업.js
+++ 프로그래머스/unrated/181920. 카운트 업/카운트 업.js
@@ -1,9 +1,9 @@
-function solution(start_num, end_num) {
+function solution(start, end) {
     let answer = [];
-    if((start_num >= 0 && start_num <= end_num) && (end_num <= 50)) { // 제한사항
+    if((start >= 0 && start <= end) && (end <= 50)) { // 제한사항
         
-        for(let i = 0; i <= (end_num - start_num); i++) {
-            answer.push(start_num + i);
+        for(let i = start; i <= end; i++) {
+            answer.push(i);
         }
     }
     return answer;
