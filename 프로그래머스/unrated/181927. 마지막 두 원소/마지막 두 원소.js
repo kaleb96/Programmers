18f995313aa0c5a387a2b64d2fffa00c75a366cc
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-11-30 11:15:14 +0900

    Update 마지막 두 원소.js

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,14 +1,18 @@
 function solution(num_list) {
  
     let num = num_list.length;
-    let endNum = num_list[num - 1];
-    let endBeforeNum = num_list[num - 2];
+    let endNum = num_list[num - 1]; // 마지막 원소
+    let endBeforeNum = num_list[num - 2]; // 그 전 원소
     
-    if(endNum > endBeforeNum) {
-        num_list.splice(num, 1, endNum-endBeforeNum);
-        return num_list;
-    } else {
-        num_list.splice(num, 1, (endNum*2));
-        return num_list;
+    if(num>=2 && num <= 10) { // 제한사항
+        
+        if(endNum > endBeforeNum) { //마지막원소가 그 전 원소보다 클 경우
+            num_list.splice(num, 1, endNum-endBeforeNum);
+            return num_list;
+        } else { // 마지막 원소가 그 전 원소보다 작을 경우
+            num_list.splice(num, 1, (endNum*2));
+            return num_list;
+        }
     }
+
 }
