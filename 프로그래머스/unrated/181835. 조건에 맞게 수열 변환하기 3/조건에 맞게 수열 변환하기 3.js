df252333dd616df134e99ffc1a0bd7ef14143f33
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-12-01 14:07:23 +0900

    Update 조건에 맞게 수열 변환하기 3.js

--- 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
@@ -7,7 +7,7 @@ function solution(arr, k) {
                   
         if(arr.length>=1 && arr.length <=1000000) { // 제한사항1
            
-            // if(minNum >= 1 && maxNum <= 100) {
+            // if(minNum >= 1 && maxNum <= 100) { // 제한사항 1-1
                 
                 for(num of arr) {
                 
