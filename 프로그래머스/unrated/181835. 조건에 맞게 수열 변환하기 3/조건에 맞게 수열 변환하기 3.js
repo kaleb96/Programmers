8403e6f367a6c4897c3207e41b040e05444f2b9c
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-12-01 14:06:46 +0900

    [level unrated] Title: 조건에 맞게 수열 변환하기 3, Time: 9.93 ms, Memory: 47 MB -BaekjoonHub

--- 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
+++ 프로그래머스/unrated/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.js
@@ -1,18 +1,24 @@
 function solution(arr, k) {
     let answer = [];
-
-    // if(k>=1 && k<=100) { // 제한사항2
+    // let maxNum = Math.max(...arr);
+    // let minNum = Math.min(...arr);
+    
+    if(k>=1 && k<=100) { // 제한사항2
                   
-        // if((arr.length>=1 && arr.length <=1000000) && (Math.min(...arr) >= 1 && Math.max(...arr) <= 100)) { // 제한사항1
-                              
-            for(let i=0; i < arr.length; i++) {
+        if(arr.length>=1 && arr.length <=1000000) { // 제한사항1
+           
+            // if(minNum >= 1 && maxNum <= 100) {
                 
-                if(k%2 === 1) 
-                    answer[i] = arr[i] * k;
-                else 
-                    answer[i] = arr[i] + k;
-            // }     
-        // }
+                for(num of arr) {
+                
+                    if(k%2 === 1) 
+                        answer.push(num * k);
+                    else 
+                        answer.push(num + k);
+                }   
+            // }
+  
+        }
     }
     return answer;
 }
