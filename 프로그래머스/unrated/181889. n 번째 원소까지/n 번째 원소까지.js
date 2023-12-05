62376daad535e710c113994b6b1f741b91a8da6f
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-12-05 14:56:57 +0900

    [level unrated] Title: n 번째 원소까지, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.js
@@ -0,0 +1,21 @@
+function solution(num_list, n) {
+    let answer = [];
+    let len = num_list.length;
+    let min = Math.min(...num_list);
+    let max = Math.max(...num_list);
+    
+    if((len >= 2 && len <= 30) && (min >= 1 && max <= 9)) {
+        
+        if(n>=1 && n<= len) {
+            
+            for(let i = 1; i<=n; i++){
+                answer.push(num_list[i - 1]);
+            }
+             // answer = num_list.slice(0, n);
+        }
+
+    }
+    
+  
+    return answer;
+}
