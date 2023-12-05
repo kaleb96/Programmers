a2882d52da45d6fc17262d7aa30ceab12e498afa
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-12-05 15:15:20 +0900

    [level unrated] Title: 문자열의 뒤의 n글자, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.js
+++ 프로그래머스/unrated/181910. 문자열의 뒤의 n글자/문자열의 뒤의 n글자.js
@@ -2,11 +2,14 @@ function solution(my_string, n) {
     let answer = '';
     let len = my_string.length;
     
-    if((len >= 1 && len <= 1000) && (n >=1 && n <= len)) {
+    if((len >= 1 && len <= 1000) && (n >=1 && n <= len)) { // 제한사항
        
-        for(let i = (len - n); i < len; i++) {
-           answer += my_string[i];
-       } 
+        // for(let i = (len - n); i < len; i++) {
+        //     answer += my_string[i];
+        // } 
+        
+        answer = my_string.slice(- n);
+        
     }
 
     return answer;
