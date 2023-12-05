33ef1f63aa2466f7734b83319bd29a9013f52620
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-12-05 14:57:52 +0900

    Update n 번째 원소까지.js

--- 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.js
+++ 프로그래머스/unrated/181889. n 번째 원소까지/n 번째 원소까지.js
@@ -4,9 +4,9 @@ function solution(num_list, n) {
     let min = Math.min(...num_list);
     let max = Math.max(...num_list);
     
-    if((len >= 2 && len <= 30) && (min >= 1 && max <= 9)) {
+    if((len >= 2 && len <= 30) && (min >= 1 && max <= 9)) { // 제한사항
         
-        if(n>=1 && n<= len) {
+        if(n>=1 && n<= len) { // 제한사항
             
             for(let i = 1; i<=n; i++){
                 answer.push(num_list[i - 1]);
