7ce15ee9b1c05cc569a842dba508df608693a098
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-12-06 10:36:09 +0900

    Update 글자 이어 붙여 문자열 만들기.js

--- 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
+++ 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -16,3 +16,10 @@ function solution(my_string, index_list) {
         return index_list.map(i => my_string[i]).join('');
     }
 }
+
+/*
+자바스크립트에서 map은 배열을 순회하면서 각 요소에 대해 특정한 동작을 수행하고,
+그 결과를 새로운 배열로 반환하는 고차함수.
+
+index_list를 순회하면서 index_list[i]의 값들을 join으로 문자열로 결합하는 방식
+*/
