b87cb5bb4ae6cb217eb912131a465eec34d4b81d
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-12-06 10:33:16 +0900

    [level unrated] Title: 글자 이어 붙여 문자열 만들기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
+++ 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.js
@@ -6,11 +6,13 @@ function solution(my_string, index_list) {
     let letIdx = index_list.length;
     
     if((lenStr >= 1 && lenStr<= 1000) && (letIdx >= 1 && letIdx <= 1000) && (min >= 0 && max <= lenStr)) { // 제한사항
-        for(let i = 0; i<index_list.length; i++) {
+        
+//         for(let i = 0; i<index_list.length; i++) {
 
-            let idx = index_list[i];
-            answer += my_string[idx];
-        }
-        return answer;
+//             let idx = index_list[i];
+//             answer += my_string[idx];
+//         }
+//         return answer;
+        return index_list.map(i => my_string[i]).join('');
     }
 }
