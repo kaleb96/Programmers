3f90c275b57c4ef302adc21007d32f974a3fe0fe
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-12-07 13:15:39 +0900

    [level unrated] Title: 주사위 게임 1, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
+++ 프로그래머스/unrated/181839. 주사위 게임 1/주사위 게임 1.js
@@ -2,19 +2,17 @@ const solution = (a, b) => {
     
     if((a>=1 && a<=6) && (b>=1 && b<=6)) { // 제한사항
         
-        
-        if((a + b) % 2 === 1) { // 정수 a, b 중 하나만 홀수
-            
+        if((a + b) % 2 === 1)  // 정수 a, b 중 하나만 홀수
             return 2 * (a + b);
-        } else { // 두 정수 모두 홀수, 짝수 인 경우
+        
+        else { // 두 정수 모두 홀수, 짝수 인 경우
             
-            if(a % 2 === 1) {// 모두 홀수 인 경우 
-                
-                return (a * a) + (b * b);
-            } else {
-                
+            if(a % 2 === 1)  // 모두 홀수 인 경우 
+                // return (a * a) + (b * b);
+                return Math.pow(a, 2) + Math.pow(b, 2);
+            
+             else 
                 return Math.abs(a - b);
-            }
         }
     }
 }
