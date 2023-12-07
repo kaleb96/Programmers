9444d9f7c79a771352a93e243c58e6989114b17d
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-12-07 13:59:33 +0900

    [level unrated] Title: 원소들의 곱과 합, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -1,16 +1,27 @@
-function solution(num_list) {
-    let answer = 0;
-    let multiply = 1;
-    let add = 0;
+// function solution(num_list) {
+//     let answer = 0;
+//     let multiply = 1;
+//     let add = 0;
     
-    for(let a of num_list) { //모든 원소들의 곱
-        multiply *= a;
-        add += a; // 모든 원소들의 합
-    }
+//     for(let a of num_list) { //모든 원소들의 곱
+//         multiply *= a;
+//         add += a; // 모든 원소들의 합
+//     }
     
-    // if(mutiply < Math.pow(add, 2))
-    if(multiply < (add*add))
+//     // if(mutiply < Math.pow(add, 2))
+//     if(multiply < (add*add))
+//         return 1;
+//     else 
+//         return 0;
+// }
+
+const solution = (str) => {
+    
+    let x = str.reduce((a, b) => a *= b); // 모든 원소들의 곱
+    let pow = Math.pow(str.reduce((a,b)=> a += b), 2) // 모든 원소들의 합의 제곱
+    
+    if(x < pow)
         return 1;
-    else 
+    else
         return 0;
 }
