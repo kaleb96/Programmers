cd7fc8406d6bb6cecc4605b718803a05d5c4e419
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-12-07 16:19:19 +0900

    [level unrated] Title: 마지막 두 원소, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.js
@@ -1,24 +1,18 @@
-// function solution(num_list) {
-//     var answer = [];
-//     return answer;
-// }
-
-const solution = (arr) => {
+function solution(num_list) {
+ 
+    let num = num_list.length;
+    let endNum = num_list[num - 1]; // 마지막 원소
+    let endBeforeNum = num_list[num - 2]; // 그 전 원소
     
-    let len = arr.length;
-    let end = arr[len - 1]; // 마지막 원소
-    let endBefore = arr[len - 2]; // 그 전 원소
-    
-    if(len>=2 && len <= 10) { // 제한사항
+    if(num>=2 && num <= 10) { // 제한사항
         
-        if(end > endBefore) { //마지막원소가 그 전 원소보다 클 경우
-            
-            arr.splice(len, 1, end-endBefore);
-            return arr;
-        
+        if(endNum > endBeforeNum) { //마지막원소가 그 전 원소보다 클 경우
+            num_list.splice(num, 1, endNum-endBeforeNum);
+            return num_list;
         } else { // 마지막 원소가 그 전 원소보다 작을 경우
-            arr.splice(len, 1, (end*2));
-            return arr;
+            num_list.splice(num, 1, (endNum*2));
+            return num_list;
         }
     }
+
 }
