7d862cfc925d324959437aa44f7aa6702e771786
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-12-07 14:02:49 +0900

    Update 원소들의 곱과 합.js

--- 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
+++ 프로그래머스/unrated/181929. 원소들의 곱과 합/원소들의 곱과 합.js
@@ -17,11 +17,18 @@
 
 const solution = (str) => {
     
-    let x = str.reduce((a, b) => a *= b); // 모든 원소들의 곱
-    let pow = Math.pow(str.reduce((a,b)=> a += b), 2) // 모든 원소들의 합의 제곱
+    //제한사항을 위한 변수
+    let len = str.length;
+    let min = Math.min(...str);
+    let max = Math.max(...str);
     
-    if(x < pow)
-        return 1;
-    else
-        return 0;
+    if((len >=2 && len <= 10) && (min >= 1 && max <= 9)) { // 제한사항
+        let x = str.reduce((a, b) => a *= b); // 모든 원소들의 곱
+        let pow = Math.pow(str.reduce((a,b)=> a += b), 2) // 모든 원소들의 합의 제곱
+
+        if(x < pow)
+            return 1;
+        else
+            return 0;
+    }
 }
