45197f0631f75fe03f4f137af9958da3cf71ddc4
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-12-07 10:33:47 +0900

    [level unrated] Title: 길이에 따른 연산, Time: 0.06 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181879. 길이에 따른 연산/길이에 따른 연산.js
@@ -0,0 +1,25 @@
+function solution(num_list) {
+    let answer = 0;
+    let len = num_list.length;
+    let min = Math.min(...num_list); // 원소의 최소값
+    let max = Math.max(...num_list); // 원소의 최대 값
+    
+    if((len >= 2 && len <= 20) && (min >=1 && max <=9)) { // 제한사항
+        
+        if(len >= 11) {
+            
+            for(let tmp of num_list) {
+                answer += tmp;  
+            }
+        }
+        
+        if(len <= 10) {
+            
+            answer = 1;
+            for(let tmp of num_list) {
+                answer *= tmp;
+            }
+        }
+    }
+    return answer;
+}
