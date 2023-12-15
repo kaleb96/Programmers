dcdf437713f8489f5a15bca39ef87d95a357c11f
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-12-15 13:30:15 +0900

    [level unrated] Title: 부분 문자열인지 확인하기, Time: 0.04 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.js
+++ 프로그래머스/unrated/181843. 부분 문자열인지 확인하기/부분 문자열인지 확인하기.js
@@ -1,9 +1,24 @@
 const solution = (str, target) => {
 
+    // includes 를 이용한 방식
     let result = str.includes(target);
     
     if(result)
         return 1;
     else 
         return 0;
+    
+    /* 
+    RegExp는 JavaScript에서 정규식 표현을 나타내는 객체,
+    test 메서드를 통해서 RegExp 객체의 문자열을 정규식패턴과 일치하는지 판단하여 
+    일치 여부에 따라 true/false값을 리턴
+    */
+//     let result = new RegExp(`${target}`).test(str);
+    
+//     if(result)
+//         return 1;
+//     else 
+//         return 0
+    
+
 }
