6a0a699301f578237243399a19aa60088c18a574
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-12-15 10:10:21 +0900

    [level unrated] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -1,18 +1,11 @@
-function solution(num_list) {
+const solution = (arr) => {
     let answer = 0;
-    let len = num_list.length;
-    let max = Math.max(...num_list);
-    let min = Math.min(...num_list);
     
-    if((len >= 5 && len <= 100) && (min >= -10 && max <= 100)) { // 제한사항
-    
-        for(let i = 0; i < len; i++) {
+    for(let i = 0; i < arr.length; i++) {
 
-            answer = num_list[i];
-
-            if(answer < 0) //음수가 있을경우 인덱스리턴
-            return i;
-        }
-        return -1; // 없을 경우 -1리턴
+        if(arr[i] < 0) //음수가 있을경우 인덱스리턴
+        return i;
     }
+    return -1; // 없을 경우 -1리턴
 }
+
