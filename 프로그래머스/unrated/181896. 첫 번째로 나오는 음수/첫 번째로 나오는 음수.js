620b63d83c6dfd725e8371fc2fd912a3d7cdb533
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2023-12-15 10:24:42 +0900

    Update 첫 번째로 나오는 음수.js

--- 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
+++ 프로그래머스/unrated/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.js
@@ -9,10 +9,35 @@
 //     return -1; // 없을 경우 -1리턴
 // }
 
+// const solution = (arr) => {
+
+    /*
+        findIndex 메서드는 주어진 함수의 조건을 만족하는
+        배열 요소의 인덱스를 찾는데, 조건에 만족하는 요소가 없다면
+        -1을 반환 합니다.
+    */
+//     const idx = arr.findIndex(x => x < 0);
+//     return idx;
+// }
+
 const solution = (arr) => {
     
-    const idx = arr.findIndex(x => x < 0);
-    
-    return idx;
+    /*
+        filter 메서드는
+        배열에서 주어진 함수의 조건을 만족하는 모든 요소를 
+        포함하는 새로운 배열을 생성합니다.
+    */
+    let newArr = arr.filter((c) => { 
+        
+        if(c < 0) {
+            return c
+        }
+    })
+      
+    return arr.indexOf(newArr[0]); 
+    /*
+    filter으로 찾은 값을 배열로 리턴하기 때문에 배열의 첫번째값을 리턴, 
+    indexOf를 통해 값이 없을 경우 -1 을 리턴
+    */
 }
 
