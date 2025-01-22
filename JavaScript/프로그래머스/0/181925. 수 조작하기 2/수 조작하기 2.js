234e5e35f0fa5e23375be10362e91207f9efb6ad
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2025-01-22 17:16:24 +0900

    [level 0] Title: 수 조작하기 2, Time: 11.09 ms, Memory: 40.8 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
+++ JavaScript/프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.js
@@ -1,9 +1,47 @@
-function solution(numLog) {
-    const convert = {
-        '1': 'w', '-1': 's', '10': 'd', '-10': 'a'
-    };
-
-    return numLog.slice(1).map((v, i) => {
-        return convert[v - numLog[i]]
-    }).join('')
+function calculateKey (num) {
+    switch(num) {
+        case 1: {
+            return 'w';
+        }
+        case -1: {
+            return 's';
+        }
+        case 10:{
+            return 'd'
+        }
+        default: {
+            return 'a';
+        }
+    }
 }
+
+function solution(numLog) {
+    const firstNum = numLog[0];
+    let insertKey = '';
+    numLog.forEach((num, idx) => {
+        switch(idx) {
+            case 0: {
+                break;
+            }
+            case 1: {
+                insertKey += calculateKey(num - firstNum);
+                break;
+            }
+            default: {
+                insertKey += calculateKey(num - numLog[idx - 1])
+                break;
+            }
+        }
+    })
+    return insertKey;
+}
+
+// function solution(numLog) {
+//     const convert = {
+//         '1': 'w', '-1': 's', '10': 'd', '-10': 'a'
+//     };
+
+//     return numLog.slice(1).map((v, i) => {
+//         return convert[v - numLog[i]]
+//     }).join('')
+// }
