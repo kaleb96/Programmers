28a05d48a596cb2bc34f69185103a5e6f2bb2b4f
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2025-01-22 15:34:57 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
+++ JavaScript/프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.js
@@ -1,7 +1,8 @@
 function solution(str, k) {
-    let result = '';
-    for(let i = 0; i < k; i++) {
-        result += str;
-    }
-    return result;
+    // let result = '';
+    // for(let i = 0; i < k; i++) {
+    //     result += str;
+    // }
+    // return result;
+    return str.repeat(k)
 }
