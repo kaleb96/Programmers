3e6388e41f23e4755234209a4d6f8acbbad8308c
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2025-01-23 10:51:59 +0900

    Update 조건에 맞게 수열 변환하기 1.js

--- JavaScript/프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
+++ JavaScript/프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.js
@@ -3,13 +3,10 @@ function solution(arr) {
         if(v >= 50 && v % 2 === 0) {
             return v / 2;
         } 
-        
         if( v < 50 && v % 2 === 1) {
             return 2 * v;
         }
-        else {
-            return v;  
-        }
+        return v;  
     })
     return result;
 }
