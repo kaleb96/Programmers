70c75f3d33421dbeaa7d3405aa5409ec0cbdfa64
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2025-02-06 13:27:23 +0900

    [level 0] Title: 문자열 뒤집기, Time: 0.10 ms, Memory: 33.5 MB -BaekjoonHub

--- JavaScript/프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
+++ JavaScript/프로그래머스/0/181905. 문자열 뒤집기/문자열 뒤집기.js
@@ -1,7 +1,6 @@
 function solution(my_string, s, e) {
     const str = [...my_string];
     const subStr = my_string.substring(s, e + 1);
-    console.log('subStr = ', subStr)
     const reversedStr = [...subStr].reverse();
     const splicedStr = str.splice(s, e - s + 1, ...reversedStr);
     return str.join('');
