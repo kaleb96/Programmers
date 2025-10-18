98563d8a1a6faab1fb8123eafb2c636fc0db71c1
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2025-10-18 15:53:15 +0900

    Update 크기가 작은 부분문자열.js

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -1,3 +1,18 @@
+// 소스 품질 개선을 위한 수정답안
+function solution(t, p) {
+  const lenP = p.length;
+  const lenT = t.length;
+  let count = 0;
+
+  for (let i = 0; i <= lenT - lenP; i++) {
+    const sub = t.slice(i, i + lenP);
+    if (sub <= p) count++;
+  }
+
+  return count;
+}
+
+/* 기본답안
 function solution(t, p) {
     // p와 길이
     // p와 동일한 길이의 숫자 출력
@@ -20,3 +35,4 @@ function solution(t, p) {
     
     return count;
 }
+*/
