ab5ff501fc1824b68499edc7dd79b513bceea1dc
kaleb96 <97508453+kaleb96@users.noreply.github.com>
2025-10-18 15:48:34 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 469.63 ms, Memory: 37.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.js
@@ -0,0 +1,22 @@
+function solution(t, p) {
+    // p와 길이
+    // p와 동일한 길이의 숫자 출력
+    // 작거다 같은 것들의 횟수 체크
+    // t의 길이만큼 반복
+    const lenP = p.length;
+    const lenT = t.length;
+    let count = 0;
+    for (let i = 0; i < lenT; i++) {
+        const splited = [...t].splice(i,lenP).join('');
+        if(splited.length === lenP) {
+            // 비교
+            const flag = parseInt(splited);
+            const parseP = parseInt(p);
+            if(flag <= parseP) {
+                count += 1;
+            }
+        }
+    }    
+    
+    return count;
+}
